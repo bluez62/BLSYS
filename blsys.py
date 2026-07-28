345263435bb496730d1db52216effab824631d9c
bluez62 <107633296+bluez62@users.noreply.github.com>
2026-07-28 16:45:04 -0500

    Refactor blsys.py to add input handling and calculations

--- blsys.py
+++ blsys.py
@@ -1,9 +1,17 @@
+#Python
+
 level = 0
 levelreq = 0
 blsys = {}
 
 def var(var_name, var_value, storage_dict):
-    storage_dict[var_name] = var_value
+    blsys[var_name] = var_value
+
+def InputVar(var_name, text, bType, blsys):
+    if bType == int:
+        blsys[var_name] = int(input(text))
+    if bType == str:
+        blsys[var_name] = str(input(text))
 
 def DisplayMessage(x):
     if level == levelreq:
@@ -14,6 +22,28 @@ def exit_block():
     levelreq -= 1
     if level > levelreq:
         level -= 1
+
+def blsysIf(var_name, operator, value, actions):
+    global levelreq, level
+    levelreq += 1
+    
+    def check_condition():
+        current_val = blsys[var_name]
+        if operator == "<=": return current_val <= value
+        if operator == "==": return current_val == value
+        if operator == ">=": return current_val >= value
+        if operator == ">":  return current_val > value
+        if operator == "<":  return current_val < value
+        return False
+
+    if check_condition():
+        if level == (levelreq - 1):
+            level += 1
+            for action in actions:
+                action()
+            level -= 1
+            
+    exit_block()
 
 def blsysWhile(var_name, operator, value, actions):
     global levelreq, level
@@ -37,19 +67,27 @@ def blsysWhile(var_name, operator, value, actions):
             
     exit_block()
 
+def wait(time):
+    pass
 
 
 
 
+#BLSYS
+
+def plusCalc(): DisplayMessage(blsys["firstNumber"] + blsys["secondNumber"])
+def minusCalc(): DisplayMessage(blsys["firstNumber"] - blsys["secondNumber"])
+def multCalc(): DisplayMessage(blsys["firstNumber"] * blsys["secondNumber"])
+def divideCalc(): DisplayMessage(blsys["firstNumber"] / blsys["secondNumber"])
+
+InputVar("firstNumber", "First Number: ", int, blsys)
+InputVar("operator", "+, -, X, or /: ", str, blsys)
+InputVar("secondNumber", "Second Number: ", int, blsys)
+
+blsysIf("operator", "==", "+", [plusCalc])
+blsysIf("operator", "==", "-", [minusCalc])
+blsysIf("operator", "==", "X", [multCalc])
+blsysIf("operator", "==", "/", [divideCalc])
 
 
-
-var("countdown", 10, blsys)
-
-def decrement(): var("countdown", blsys["countdown"] - 1, blsys)
-def show_num(): DisplayMessage(blsys["countdown"])
-def blast_off(): DisplayMessage("Blast off!")
-
-blsysWhile("countdown", ">=", 1, [show_num, decrement])
-blsysIf("countdown", "==", 0, [blast_off])
 
